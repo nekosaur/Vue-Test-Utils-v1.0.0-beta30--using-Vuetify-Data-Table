98c4d8040888d753ea5c37a5599eceb28ca0fda1
Austino18 <austinodavis@gmail.com>
2020-01-16 10:07:41 +0100

    Create EmployeeTable.vue

---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-data-table
+    id="permission-table"
+    v-model="selected"
+    :headers="headers"
+    :items="value"
+    :items-per-page="10"
+    class="elevation-1"
+  >
+   
+  </v-data-table>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export const EmployeeTableObject = () => ({
+  name: 'PermissionTable',
+  props: {
+    value: Array
+  },
+  data: () => ({
+    selected: [],
+    headers: [
+      { text: 'Name', value: 'displayName', sortable: true },
+      { text: 'Department', value: 'department', sortable: true }
+    ],
+  })
+});
+
+export default EmployeeTableObject();
+export const EmployeeTable = Vue.component();
+
+</script>
+
+<style lang="scss" scoped>
+#permission-table {
+  width: 100%;
+}
+
+.cb-center {
+  margin-left: auto;
+}
+
+.icon-clickable {
+  cursor: pointer;
+  color: #222;
+
+  :hover {
+    color: orange;
+    transition: color 200ms ease-out;
+  }
+}
+</style>
