93ca9916d73cd15c645578ea110c34e1c1a86407
Austino18 <austinodavis@gmail.com>
2020-01-16 10:06:44 +0100

    Create service.getExistingUsers.js

---
+++ src/services/service.getExistingUsers.js
@@ -0,0 +1,21 @@
+const users = [
+  {
+    displayName: 'Max Peter', 
+    department: 'HR',
+    _id: '001'
+  },
+  {
+    displayName: 'Katie', 
+    department: 'Marketing',
+    _id: '002'
+  },
+  {
+    displayName: 'Chan Chung', 
+    department: 'HR',
+    _id: '003'
+  },
+];
+
+export default async () => {
+  return users;
+};
